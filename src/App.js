2c06bcadec5bc409cfc735cb55d708f4b99c02cf
james-owen <james@jamesowen.io>
2019-02-22 11:01:37 -0500

    Stories to function component

--- src/App.js
+++ src/App.js
@@ -8,11 +8,41 @@ class App extends React.Component {
     this.state = {};
   }
 
+  saveArticles(articles) {
+    articles0 = [];
+    articles1 = [];
+    articles2 = [];
+    
+    for (let i = 0; i < articles.length; i += 3) {
+      if (articles[i]) {
+        articles0.push(articles[i]);
+      } else break;
+      
+      if (articles[i+1]){
+        articles1.push(articles[i + 1]);
+      } else break;
+
+      if (articles[i+2]){
+        articles2.push(articles[i+2]);
+      } else break;
+    }
+
+    this.setState({
+      articles1: articles1,
+      articles2: articles2,
+      articles3: articles3
+    })
+    
+  }
+
   render() {
     return(
     <div>
     <SearchBar></SearchBar>
-    <Stories></Stories>
+    <Stories 
+      articles0={articles0}
+      articles1={articles1}
+      articles2={articles2}></Stories>
     </div>
 
     )
--- src/stories.js
+++ src/stories.js
@@ -11,50 +11,17 @@ function Story(props) {
     )
 }
 
-class Stories extends React.Component {  
-  constructor(props) {
-    super(props);
-    this.state = {articles:[
-      {
-        "source": {
-          "id": "mashable",
-          "name": "Mashable"
-        },
-        "author": "0 Sage Anderson",
-        "title": "WeRateDogs is releasing a new card game where you can rate all the good pups",
-        "description": "Have you every spotted a dog in the wild and thought, \"Wow, that is a certified Good Boy, 13/10, would recommend?\" Then this is the perfect card game for you. The viral Twitter account WeRateDogs, or, \"Your Only Source For Professional Dog Ratings,\" according…",
-        "url": "https://mashable.com/article/weratedogs-card-game-dogs/",
-        "urlToImage": "https://mondrian.mashable.com/2019%252F02%252F07%252F37%252Fe6fbdc0a56444996bfea30d695882c82.705a9.jpg%252F1200x630.jpg?signature=Ljq4QL7u7tstZOAVqvPx5T554yE=",
-        "publishedAt": "2019-02-07T19:13:26Z",
-        "content": "Have you every spotted a dog in the wild and thought, \"Wow, that is a certified Good Boy, 13/10, would recommend?\" Then this is the perfect card game for you. \r\nThe viral Twitter account WeRateDogs , or, \"Your Only Source For Professional Dog Ratings,\" accord… [+1428 chars]"
-      },
-      {
-        "source": {
-          "id": "mashable",
-          "name": "Mashable"
-        },
-        "author": "1 Sage Anderson",
-        "title": "WeRateDogs is releasing a new card game where you can rate all the good pups",
-        "description": "Have you every spotted a dog in the wild and thought, \"Wow, that is a certified Good Boy, 13/10, would recommend?\" Then this is the perfect card game for you. The viral Twitter account WeRateDogs, or, \"Your Only Source For Professional Dog Ratings,\" according…",
-        "url": "https://mashable.com/article/weratedogs-card-game-dogs/",
-        "urlToImage": "https://mondrian.mashable.com/2019%252F02%252F07%252F37%252Fe6fbdc0a56444996bfea30d695882c82.705a9.jpg%252F1200x630.jpg?signature=Ljq4QL7u7tstZOAVqvPx5T554yE=",
-        "publishedAt": "2019-02-07T19:13:26Z",
-        "content": "Have you every spotted a dog in the wild and thought, \"Wow, that is a certified Good Boy, 13/10, would recommend?\" Then this is the perfect card game for you. \r\nThe viral Twitter account WeRateDogs , or, \"Your Only Source For Professional Dog Ratings,\" accord… [+1428 chars]"
-      }
-    ]};
-  }
-
-  render() {
-    return(
-      <div class="tile is-ancestor">
-        <div class="tile is-parent is-4 is-vertical">
-        {this.state.articles.map(article => {
-          return <Story article={article}></Story>
-        })}
-        </div>
+function Stories(props) {  
+  return(
+    <div class="tile is-ancestor">
+      <div class="tile is-parent is-4 is-vertical">
+      {this.props.articles0.map(article => {
+        return <Story article={article}></Story>
+      })}
       </div>
-    )
-  }
+    </div>
+  )
 }
+
 
 export default Stories
